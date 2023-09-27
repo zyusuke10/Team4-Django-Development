8b97426f27d8e3f3518b870f80c603b26e81bd41
zyusuke <zyusuke10@gmail.com>
2023-09-27 17:08:30 +0900

    "Created a read function to get product information from the cart

--- team_4_app/urls.py
+++ team_4_app/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
+from . import views
+
 app_name = 'team_4_app'
 urlpatterns = [
+    path('checkout/',views.CartListView.as_view(),name='checkout'),
 ]
--- team_4_app/views.py
+++ team_4_app/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from .models import Cart
+from django.views.generic import ListView
+import requests
+import os
 
 # Create your views here.
+
+class CartListView(ListView):
+    model = Cart
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart_items = list(context['object_list'].values_list('product_id', flat=True))
+        product_info_list = []
+        
+        rakuten_api_endpoint = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601?"
+        rakuten_applicationId = os.getenv('APPLICATION_ID')
+        
+        for cart_item in cart_items:
+            product_id = cart_item
+            params = {
+                    'applicationId':rakuten_applicationId,
+                    'itemCode': product_id, 
+            }
+            response = requests.get(rakuten_api_endpoint,params=params)
+
+            if response.status_code == 200:
+                product_info = response.json()
+                product_info_list.append(product_info)
+                
+        context['product_info_list'] = product_info_list
+        print(product_info_list)
+        
+        return context
