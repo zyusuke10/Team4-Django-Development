ec3e855f9fde1b58068a7d86597092cb3939eaf8
Jovan Frandika <58182201+jovanfrandika@users.noreply.github.com>
2023-09-27 21:31:10 +0700

    feat: Create Promotional Video List Page

--- team_4_app/urls.py
+++ team_4_app/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path
+
+from . import views
+
 app_name = 'team_4_app'
 urlpatterns = [
+    path('promotional-video/', views.PromotionalVideoListView.as_view(), name='promotional_video_list'),
 ]
--- team_4_app/views.py
+++ team_4_app/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import PromotionalVideo
+
+class PromotionalVideoListView(ListView):
+    model = PromotionalVideo
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['shop_stories'] = [
+            {
+                'image_url': 'https://www.rakuten.ne.jp/gold/rakutenmobile-store/assets/img/common/logo-rmobile-1line.svg?200424',
+                'shop_code': 'rakutenmobile-store'
+            },
+            {
+                'image_url': 'https://www.rakuten.ne.jp/gold/rakutenmobile-store/assets/img/common/logo-rmobile-1line.svg?200424',
+                'shop_code': 'rakutenmobile-store'
+            },
+            {
+                'image_url': 'https://www.rakuten.ne.jp/gold/rakutenmobile-store/assets/img/common/logo-rmobile-1line.svg?200424',
+                'shop_code': 'rakutenmobile-store'
+            },
+            {
+                'image_url': 'https://www.rakuten.ne.jp/gold/rakutenmobile-store/assets/img/common/logo-rmobile-1line.svg?200424',
+                'shop_code': 'rakutenmobile-store'
+            },
+            {
+                'image_url': 'https://static.vecteezy.com/system/resources/previews/010/994/232/non_2x/nike-logo-black-clothes-design-icon-abstract-football-illustration-with-white-background-free-vector.jpg',
+                'shop_code': 'nike-official'
+            }
+        ]
+
+        return context
