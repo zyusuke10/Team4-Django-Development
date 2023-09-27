f1459639c5575938efa55716485e42643f58f549
Jovan Frandika <58182201+jovanfrandika@users.noreply.github.com>
2023-09-27 10:30:59 +0700

    feat: Create a promotional video model

--- team_4_app/models.py
+++ team_4_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class PromotionalVideo(models.Model):
+    title = models.CharField('title', max_length=100, )
+    description = models.TextField('description')
+    video_url = models.TextField('video_url')
+    shop_code = models.CharField('shop_code', max_length=100, )
+
+    def __str__(self):
+        return self.title
