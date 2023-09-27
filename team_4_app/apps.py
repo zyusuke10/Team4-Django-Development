055d032edb0460b64c8fcdf6b3367ac5c5187445
Jovan Frandika <58182201+jovanfrandika@users.noreply.github.com>
2023-09-27 10:18:05 +0700

    feat: Add team 4 app

---
+++ team_4_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Team4AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'team_4_app'
---
+++ team_4_app/urls.py
@@ -0,0 +1,3 @@
+app_name = 'team_4_app'
+urlpatterns = [
+]
