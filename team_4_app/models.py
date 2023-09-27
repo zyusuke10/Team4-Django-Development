71f337f9afc7c81b8c01b3ef2a724d5d98a060ae
zyusuke <zyusuke10@gmail.com>
2023-09-27 12:31:38 +0900

    Created a cart model

--- team_4_app/models.py
+++ team_4_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Cart(models.Model):
+    product_id = models.CharField('rakuten_product_id', max_length=100, )
+
+    def __str__(self):
+        return self.product_id
+
