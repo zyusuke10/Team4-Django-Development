51d7c360abcfae894aea0df7d532828225914a13
knafil12201 <ryo01251019@akane.waseda.jp>
2023-09-28 11:27:25 +0900

    Fixed the conflict

--- team_4_app/urls.py
+++ team_4_app/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 app_name = 'team_4_app'
 urlpatterns = [
-    #path('test_detail/<int:pk>/', views.DetailTestView.as_view(), name='test_detail'),
+    path('checkout/', views.CartListView.as_view(), name='checkout'),
+    path('shop/<str:shop_code>', views.ShopProductListView.as_view(), name='shop-product-list'),
+    path('promotional-video/', views.PromotionalVideoListView.as_view(), name='promotional-video-list'),
+    path('promotional-video/<int:pk>', views.PromotionalVideoDetailView.as_view(), name='promotional-video-detail'),
     path('delete_item_from_cart/<int:pk>/<str:product_id>/', views.delete_item_from_cart, name='delete_item_from_cart'),
-    path('product_list', views.ProductListView.as_view(), name='product_list'),
+    path('product_list/', views.ProductListView.as_view(), name='product_list'),
 ]
--- team_4_app/views.py
+++ team_4_app/views.py
@@ -1,11 +1,65 @@
-from typing import Any
+import requests
+import os
+
 from django.shortcuts import get_object_or_404, redirect
-from .models import PromotionalVideo, Cart
 from django.views.decorators.http import require_POST
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
+
+from .models import PromotionalVideo, Cart
 from .get_products_list import get_products_list
+from .constants import shop_stories
 
 # Create your views here.
+class CartListView(ListView):
+    model = Cart
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart_items = list(context['object_list'].values_list('product_id', flat=True))
+        product_info_list = []
+
+        rakuten_api_endpoint = 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601?'
+        rakuten_applicationId = os.getenv('APPLICATION_ID')
+
+        for cart_item in cart_items:
+            product_id = cart_item
+            params = {
+                'applicationId': rakuten_applicationId,
+                'itemCode': product_id,
+            }
+            response = requests.get(rakuten_api_endpoint,params=params)
+
+            if response.status_code == 200:
+                product_info = response.json()
+                product_info_list.append(product_info)
+
+        context['product_info_list'] = product_info_list
+        print(product_info_list)
+
+        return context
+
+class ShopProductListView(TemplateView):
+    template_name = 'team_4_app/shopproduct_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        shop_code = self.kwargs['shop_code']
+
+        rakuten_api_endpoint = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601?"
+        rakuten_applicationId = os.getenv('APPLICATION_ID')
+
+        params = {
+                    'applicationId':rakuten_applicationId,
+                    'shopCode': shop_code,
+            }
+        response = requests.get(rakuten_api_endpoint,params=params)
+
+        if response == 200:
+            data = response.json()
+            context['shop_data'] = data
+
+        return context
+
 @require_POST
 def delete_item_from_cart(request, pk, product_id):
     print(request.POST)
@@ -20,24 +74,33 @@ def delete_item_from_cart(request, pk, product_id):
 
     return redirect('team_4_app:', pk=pk)
 
+class ShopProductListView(TemplateView):
+    template_name = 'team_4_app/shopproduct_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class PromotionalVideoListView(ListView):
+    model = PromotionalVideo
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['shop_stories'] = shop_stories
+
+        return context
+
+class PromotionalVideoDetailView(DetailView):
+    model = PromotionalVideo
+    template_name = 'team_4_app/promotionalvideo_detail.html'
+
 class ProductListView(TemplateView):
     model = Cart
     template_name = 'team_4_app/product_list.html'
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+    def get_context_data(self, **kwargs) -> dict[str]:
         context = super().get_context_data(**kwargs)
         shop_code = self.request.GET.get(key='shop_code', default='grazia-doris')
         products = get_products_list(shop_code)
         context['products'] = products
         return context
-
-class DetailTestView(DetailView):
-    model = Cart
-    template_name = 'team_4_app/test_detail.html'
-
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        content = super().get_context_data(**kwargs)
-        cart = content['cart']
-        ids = cart.product_id
-        ids = ids.split(',')
-        content['cart'].product_id = ids
-        return content
