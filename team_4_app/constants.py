4bcacfdba5872eff69e08b49a1aa3d37f0ae7426
Jovan Frandika <58182201+jovanfrandika@users.noreply.github.com>
2023-10-09 16:04:24 +0700

    fix: Fix one-click checkout flow

--- team_4_app/constants.py
+++ team_4_app/constants.py
@@ -1,3 +1,5 @@
+SHIPPING_FEE = 400
+
 shop_code_to_shop_image_url_dict = {
     'rakutenmobile-store': 'https://www.rakuten.ne.jp/gold/rakutenmobile-store/assets/img/common/logo-rmobile-1line.svg?200424',
     'nike-official': 'https://static.vecteezy.com/system/resources/previews/010/994/232/non_2x/nike-logo-black-clothes-design-icon-abstract-football-illustration-with-white-background-free-vector.jpg'
